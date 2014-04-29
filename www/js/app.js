258b4b708bd9edfdd8bec0f8666e04ecd022e237
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-04-29 10:23:21 -0400

    Try to remove some of the stutter from our lightbox.

--- www/js/app.js
+++ www/js/app.js
@@ -338,8 +338,15 @@ var lightboxImage = function(element) {
     });
 
     // Transition with debounce.
-    fade = _.debounce(fadeLightboxIn, 1);
+    fade = _.debounce(fadeLightboxIn, 100);
     fade();
+
+    // Never looks good to have scroll bars appear, adding
+    // several pixels of padding to the body. Make this match
+    // the fade in above.
+    _.delay(function(){
+        $('body').css({ overflow: 'hidden' });
+    }, 250);
 
     // Grab Wes's properly sized width.
     var lightbox_width = w;
@@ -386,11 +393,6 @@ var lightboxImage = function(element) {
         'left': lightbox_left + 'px',
     });
 
-    // Disable scrolling while the lightbox is present.
-    $('body').css({
-        overflow: 'hidden'
-    });
-
     // On click, remove the lightbox.
     $lightbox.on('click', onRemoveLightbox);
 };
@@ -408,23 +410,24 @@ var onRemoveLightbox = function() {
         opacity: 0,
     });
 
-    // Un-disable scrolling.
-    $('body').css({
-        overflow: 'auto'
-    });
-
     // Debounce the fade.
-    fade = _.debounce(fadeLightboxOut, 250);
+    fade = _.debounce(fadeLightboxOut, 100);
     fade();
+
+    // Never looks good to have scroll bars appear, adding
+    // several pixels of padding to the body. Make this match
+    // the fade out above.
+    _.delay(function(){
+        $('body').css({ overflow: 'auto' });
+    }, 100);
+
 };
 
 var fadeLightboxIn = function() {
     /*
     * Fade in event.
     */
-    $lightbox.css({
-        opacity: 1
-    });
+    $lightbox.css({ opacity: 1 });
 };
 
 var fadeLightboxOut = function() {
